7f01592dd5a35c806a9607ece0a5ac25e297ea03
redlady95 <laurasedok76@gmail.com>
2019-03-19 21:25:59 -0500

    please fix me

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,4 @@
-container {
 
-}
 
 // Get the video
 var video = document.getElementById("myVideo");
@@ -20,5 +18,24 @@ function myFunction() {
 }
 
 
-var name = prompt ("Brian");
-consol.log("hi" name);
+
+//Start of the script for the buttons in players pages
+var i = 0;
+$("button").on("click",function() {
+  if (!i) {
+    document.getElementById("more").style.
+      display = "inline";
+    document.getElementById("dots").style.
+      display = "none";
+    document.getElementById("read").innerHTML = "Read less";
+    i = 1;
+  }
+  else {
+    document.getElementById("more").style.
+      display = "none";
+    document.getElementById("dots").style.
+      display = "inline";
+    document.getElementById("read").innerHTML = "Read more";
+    i = 0;
+  }
+})
